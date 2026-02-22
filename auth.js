36bc2fb554ffabb97c0232e6890c9d667d65569d
kaylamoon0000 <kaylamoon0000@gmail.com>
2026-02-21 23:43:58 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,14 @@
+function getCurrentUser(){
+ return localStorage.getItem("currentUser");
+}
 
+function requireLogin(){
+ if(!getCurrentUser()){
+  window.location.href="login.html";
+ }
+}
+
+function logout(){
+ localStorage.removeItem("currentUser");
+ window.location.href="login.html";
+}
